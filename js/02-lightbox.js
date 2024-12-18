3277218c68f4b62a7bc69084dd75550022936696
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-12-18 20:36:44 +0200

    go on

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const ulGallery = document.querySelector('ul.gallery');
+
+const markUp = galleryItems.map(item => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link"
+            href="${item.original}">
+            <img class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            />
+        </a>
+    </li>
+    `
+}).join('');
+// console.log(markUp);
+ulGallery.insertAdjacentHTML('beforeend', markUp);
